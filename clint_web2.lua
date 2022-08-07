68159f839d146f26b6b9e711ceb48c7cc3e66ef7
maxigzt <87970361+maxigzt@users.noreply.github.com>
2022-08-07 09:45:50 +0200

    Add files via upload

--- clint_web2.lua
+++ clint_web2.lua
@@ -1,3 +1,3 @@
 --backup plan failed so here is the original script back
 --haha sang you came here just to read this youre so curious lmao 
-return loadstring(game:HttpGet("..."))()
+return loadstring(game:HttpGet("https://raw.githubusercontent.com/maxigzt/clint/main/clint%20backup%20sytem.lua"))()
